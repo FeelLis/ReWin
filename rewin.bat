aba6e725614fc6425ac56899ab16d3008caf9190
Dmitriy <foxyteru@gmail.com>
2022-01-21 21:27:16 +0200

    Changed admin rights request

--- rewin.bat
+++ rewin.bat
@@ -1,39 +1,49 @@
 @echo off
+::::::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights ::
+::::::::::::::::::::::::::::::::::::::::::::
 
-call .\src\admin.bat
+REM  -- Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
 
+REM -- If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::::::::::::::::::::::::::::::::::::::::::::
+
+:START
 CLS
-ECHO .
 ECHO **************************************
 ECHO Starting ReWin Batch
 ECHO **************************************
 
-:START
-setlocal & pushd .
 
-WHERE choco 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto chocoInstalled ) else ( goto chocoMissing )
+:: Info about this batch script
 
-:chocoMissing
-ECHO .
-choice /M "Chocolatey is required to install the necessary programs! Install now?"
+choice /M "Install needed software"
 IF '%errorlevel%' == '2' exit /B
-
-ECHO .
-ECHO **************************************
-ECHO Installing Chocolatey
-ECHO **************************************
-
-powershell Set-ExecutionPolicy AllSigned
-powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-
-:chocoInstalled
-choco feature enable -n=allowGlobalConfirmation
 
 :: Software install
 call .\src\choco.bat
 
-choco feature disable --name=allowGlobalConfirmation
-
 :END
+pause
